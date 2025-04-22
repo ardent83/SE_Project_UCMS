3391d7737a7171c0160ec95fbc2da749d759ffc6
Ftm-Sayadzadeh <ftmdkf@gmail.com>
2025-04-22 11:25:15 +0430

    feat: add product card component

---
+++ frontend/src/classes/components/ProductCard.jsx
@@ -0,0 +1,47 @@
+import React from "react";
+import "./ProductCard.css";
+import usersIcon from "../assets/users.svg"; 
+import editIcon from "../assets/edit.svg";
+
+const ProductCard = ({
+  title,
+  instructorName,
+  studentCount,
+  imageUrl,
+  onEdit,
+}) => {
+  return (
+    <div className="product-card">
+      <div
+        className="card-image"
+        style={{ backgroundImage: imageUrl ? `url(${imageUrl})` : "none" }}
+      ></div>
+      <div className="card-content">
+        <h3>{title}</h3>
+        <h4>{instructorName}</h4>
+      </div>
+      <div className="card-footer">
+        <div className="students-number">
+          <div className="students-text">
+            <span>دانشجو</span>
+            <span>{studentCount ?? 0}</span>
+          </div>
+          <img
+            src={usersIcon}
+            alt="آیکون تعداد دانشجو"
+            className="member-icon"
+          />
+        </div>
+        <button
+          onClick={onEdit ? onEdit : () => {}}
+          className="edit-button"
+          aria-label="ویرایش"
+        >
+          <img src={editIcon} alt="آیکون ویرایش" />
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default ProductCard;
