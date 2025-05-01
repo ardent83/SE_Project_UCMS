93e3d818776865688c52a637610f046b12371d99
K-Kabiri <126855522+K-Kabiri@users.noreply.github.com>
2025-05-01 14:23:55 +0330

    feat: add email verification page to authentication.

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,13 +1,14 @@
 import React from 'react';
 import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
 import AuthPage from './auth/AuthPage';
-import ClassForm from './class-form/ClassForm';
+// import ClassForm from './class-form/ClassForm';
 import AccountSettings from './account-settings/AccountSettings';
 import Dashboard from './Dashboard';
 import { AuthProvider } from './auth/context/AuthContext';
 import PrivateRoute from './components/PrivateRoute';
 import MainLayout from './main/MainLayout';
-import ClassPage from './class-page/ClassPage';
+import VerificationPage from "./auth/email-validation/emailVerificationPage.jsx";
+// import ClassPage from './class-page/ClassPage';
 
 function App() {
   return (
@@ -15,7 +16,7 @@ function App() {
       <AuthProvider>
         <Routes>
           <Route path="/auth" element={<AuthPage />} />
-
+          <Route path="/verification" element={<VerificationPage />} />
           <Route path="/dashboard" element={
             <PrivateRoute>
               <MainLayout>
@@ -24,11 +25,11 @@ function App() {
             </PrivateRoute>
           } />
 
-          <Route path="/test" element={
-            <MainLayout>
-              <ClassForm formType={"create"} />
-            </MainLayout>
-          } />
+          {/*<Route path="/test" element={*/}
+          {/*  <MainLayout>*/}
+          {/*    <ClassForm formType={"create"} />*/}
+          {/*  </MainLayout>*/}
+          {/*} />*/}
 
           <Route path="/ue" element={
             <MainLayout>
@@ -36,11 +37,11 @@ function App() {
             </MainLayout>
           } />
 
-          <Route path="/class" element={
-            <MainLayout>
-              <ClassPage />
-            </MainLayout>
-          } />
+          {/*<Route path="/class" element={*/}
+          {/*  <MainLayout>*/}
+          {/*    <ClassPage />*/}
+          {/*  </MainLayout>*/}
+          {/*} />*/}
 
           <Route path='/' element={<Navigate to={'/class'} replace />} />
         </Routes>
