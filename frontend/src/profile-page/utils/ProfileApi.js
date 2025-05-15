9c9b2a7952adf655e1b69b3f3653dc37a8c91f90
K-Kabiri <126855522+K-Kabiri@users.noreply.github.com>
2025-05-16 01:21:09 +0330

    feat: add profile api for instructor..

--- frontend/src/profile-page/utils/ProfileApi.js
+++ frontend/src/profile-page/utils/ProfileApi.js
@@ -1,6 +1,27 @@
 const apiBaseUrl = import.meta.env.VITE_API_BASE_URL;
 
-export const handleViewProfile = async () => {
+
+export const handleViewProfileForInstructor = async () => {
+    try {
+        const res = await fetch(`${apiBaseUrl}/api/instructors/profile`, {
+            method: 'GET',
+            credentials: 'include',
+            headers: {
+                'Accept': 'application/json',
+            },
+        });
+
+        if (!res.ok) throw new Error('Error in catching profile info');
+
+        return await res.json();
+    } catch (error) {
+        console.error('Error:', error.message);
+        throw error;
+    }
+};
+
+
+export const handleViewProfileForStudent = async () => {
     try {
         const res = await fetch(`${apiBaseUrl}/api/students/profile`, {
             method: 'GET',
