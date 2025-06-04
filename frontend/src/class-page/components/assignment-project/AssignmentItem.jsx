54043eb928813e568644f014dd0cd7aeafc9ed22
K-Kabiri <126855522+K-Kabiri@users.noreply.github.com>
2025-06-05 00:08:58 +0330

    fix: some changes were made for css.

--- frontend/src/class-page/components/assignment-project/AssignmentItem.jsx
+++ frontend/src/class-page/components/assignment-project/AssignmentItem.jsx
@@ -7,8 +7,10 @@ const AssignmentItem = ({ assignment }) => {
     const daysLeft = Math.ceil((endDate - today) / (1000 * 60 * 60 * 24));
 
     return (
-        <div className="w-full max-w-84 p-4 flex flex-col justify-end items-center gap-1 rounded-lg border border-neutralgray-2 cursor-pointer">
-            <div className="w-full text-body-05 text-right text-redp flex justify-end gap-1">
+        <div className="w-full max-w-70 p-4 flex flex-col ml-6 mb-2 justify-start items-center gap-1 rounded-lg border border-neutralgray-2 cursor-pointer
+    transition-transform duration-200 ease-in-out hover:scale-[1.03] hover:shadow-md hover:bg-neutralgray-2 z-30">
+
+        <div className="w-full text-body-05 text-right text-redp flex justify-end gap-1">
                 {assignment.name}
             </div>
             <div className="w-full flex justify-end items-center gap-1 text-body-light text-right text-neutralgray-6">
