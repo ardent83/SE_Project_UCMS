03064ce8c5a5766f807374566ca9436ecf1031af
K-Kabiri <126855522+K-Kabiri@users.noreply.github.com>
2025-06-05 22:24:04 +0330

    fix: edit MemberItem.

--- frontend/src/class-page/components/member/MemberItem.jsx
+++ frontend/src/class-page/components/member/MemberItem.jsx
@@ -1,17 +1,27 @@
 import React from "react";
+import { useAuth } from "../../../auth/context/AuthContext.jsx";
 
 const MemberItem = ({ firstLastName, image }) => {
-    const hasImage = Boolean(image);
-    const namePart = firstLastName?.split("|")[1]?.trim() || "";
-    const firstChar = namePart.charAt(0).toUpperCase() || "?";
+    const { user } = useAuth();
+    const userRole = user?.role?.name || "guest";
+
+    let displayName = firstLastName;
+    let firstChar;
+
+    if (userRole === "Instructor") {
+        const namePart = firstLastName?.split("|")[1]?.trim() || "";
+        firstChar = namePart.charAt(0).toUpperCase() || "?";
+    } else {
+        firstChar = firstLastName.charAt(0).toUpperCase() || "?";
+    }
 
     return (
         <div className="w-full max-w-88 p-4 flex justify-end items-center gap-2 border-b last:border-none border-b-neutralgray-2 cursor-pointer">
             <label className="flex justify-end items-center text-body-05 text-right text-redp self-stretch cursor-pointer">
-                {firstLastName}
+                {displayName}
             </label>
 
-            {hasImage ? (
+            {image ? (
                 <div
                     className="w-8 h-8 rounded-full bg-cover bg-center"
                     style={{ backgroundImage: `url(${image})` }}
