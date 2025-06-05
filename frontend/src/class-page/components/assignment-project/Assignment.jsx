fd7da584e7f1759d723c911052697261fb63652b
K-Kabiri <126855522+K-Kabiri@users.noreply.github.com>
2025-06-05 21:28:50 +0330

    feat: connect new project and assignment icon in class page.

--- frontend/src/class-page/components/assignment-project/Assignment.jsx
+++ frontend/src/class-page/components/assignment-project/Assignment.jsx
@@ -1,11 +1,18 @@
 import HeadSection from "../HeadSection.jsx";
 import React from "react";
 import AssignmentItem from "./AssignmentItem.jsx";
+import { useNavigate } from "react-router-dom";
 
-const Assignment = ({ assignments }) => {
+const Assignment = ({ assignments, classId }) => {
+    const navigate = useNavigate();
+
+    const handleAddExercise = () => {
+        navigate(`/class/${classId}/exercise/create`);
+    };
+
     return (
         <section className="flex w-full p-3 gap-2 max-w-88 flex-col items-center flex-[1_0_0] border-[0.8px] border-solid border-neutralgray-2 rounded-lg relative">
-            <HeadSection title={"تمرین‌ها"} />
+            <HeadSection title={"تمرین‌ها"} onClick={handleAddExercise} />
             <div className="w-full max-h-150 overflow-y-auto flex flex-col gap-2 pr-5">
                 {assignments.map((assignment, index) => (
                     <AssignmentItem key={index} assignment={assignment} />
