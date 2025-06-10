48d8128e2445c2181a9f4ec8cb9c648102a4d4b2
K-Kabiri <126855522+K-Kabiri@users.noreply.github.com>
2025-06-10 17:19:07 +0330

    fix: edit font size in AboutCard and AssignmentItem.

--- frontend/src/class-page/components/AboutCard.jsx
+++ frontend/src/class-page/components/AboutCard.jsx
@@ -5,7 +5,7 @@ const AboutCard = ({ description }) => {
 
     return (
         <div className="self-stretch h-fit flex flex-col justify-center items-center rounded-lg border-[0.8px] border-solid border-neutralgray-2 p-4 " dir="rtl">
-            <h6 className="text-caption-03 text-right text-black self-stretch">درباره‌ی کلاس</h6>
+            <h6 className="text-caption-03 text-right text-[0.8rem] text-black self-stretch">درباره‌ی کلاس</h6>
             <p
                 className={` mt-4 text-caption-04 text-xs text-right self-stretch ${
                     hasDescription ? "text-black" : "text-neutral-400"
