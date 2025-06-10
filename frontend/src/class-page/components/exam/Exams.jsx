8c2a05b2c1f9d46d7023f8c897ec8a6da7bf6c41
K-Kabiri <126855522+K-Kabiri@users.noreply.github.com>
2025-06-10 17:18:30 +0330

    fix: connect exams.

--- frontend/src/class-page/components/exam/Exams.jsx
+++ frontend/src/class-page/components/exam/Exams.jsx
@@ -1,8 +1,9 @@
 import React, { useEffect, useRef } from "react";
 import Exam from "./Exam.jsx";
 import HeadSection from "../HeadSection.jsx";
+import {useNavigate} from "react-router-dom";
 
-const Exams = ({ exams }) => {
+const Exams = ({ exams , classId  }) => {
     const scrollRef = useRef(null);
 
     useEffect(() => {
@@ -11,12 +12,17 @@ const Exams = ({ exams }) => {
             container.scrollLeft = container.scrollWidth;
         }
     }, []);
+    const navigate = useNavigate();
+
+    const handleAddExam = () => {
+        navigate(`/class/${classId}/exam/create`);
+    };
 
     return (
         <div className="w-full flex flex-col items-center gap-2">
-            <HeadSection title={"امتحانات"} />
-            <div ref={scrollRef} className="w-full overflow-x-auto">
-                <div className="flex flex-row gap-4 w-fit pr-2 p-4 text-right">
+            <HeadSection title={"امتحانات"} onClick={handleAddExam} />
+            <div ref={scrollRef} className="w-full overflow-x-auto" style={{ direction: "rtl" }}>
+                <div className="flex flex-row gap-4 w-fit pl-2 p-4 text-right">
                     {exams.map((exam, index) => (
                         <div key={index} className="min-w-[150px]">
                             <Exam
@@ -31,6 +37,7 @@ const Exams = ({ exams }) => {
                 </div>
             </div>
         </div>
+
     );
 };
 
