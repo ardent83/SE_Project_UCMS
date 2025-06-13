416f344dec05eaa04987643349de7b60851b79f2
Mohsenpour <smmmpm321@gmail.com>
2025-06-13 07:39:19 +0430

    feat: add portal prop to DatePicker for improved calendar rendering

--- frontend/src/components/DatePicker.jsx
+++ frontend/src/components/DatePicker.jsx
@@ -122,6 +122,7 @@ export default function MyDatePicker({
             calendar={persian}
             locale={persian_fa}
             calendarPosition="bottom-left"
+            portal
         />
     );
 }
