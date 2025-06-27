505bf525386d814ff5acab6bb5da2fd512d59006
K-Kabiri <126855522+K-Kabiri@users.noreply.github.com>
2025-06-27 12:59:15 +0330

    fix(class-page): some changes were made on Assignment and AssignmentProject components.

--- frontend/src/class-page/components/assignment-project/AssignmentProject.jsx
+++ frontend/src/class-page/components/assignment-project/AssignmentProject.jsx
@@ -1,10 +1,10 @@
 import HeadSection from "../HeadSection.jsx";
 import React from "react";
 import AssignmentItem from "./AssignmentItem.jsx";
-import { useNavigate } from "react-router-dom";
+import {useNavigate} from "react-router-dom";
 import assignment from "./Assignment.jsx";
 
-const AssignmentProject = ({ projects, classId }) => {
+const AssignmentProject = ({projects, classId}) => {
     const navigate = useNavigate();
 
     const handleAddProject = () => {
@@ -16,17 +16,29 @@ const AssignmentProject = ({ projects, classId }) => {
     };
 
     return (
-        <section className="flex w-full p-3 gap-2 max-w-88 flex-col items-center flex-[1_0_0] border-[0.8px] border-solid border-neutralgray-2 rounded-lg ">
-            <HeadSection title={"پروژه‌ها"} onClick={handleAddProject} />
-            <div className="w-full max-h-150 overflow-y-auto flex flex-col gap-2 pr-5 relative">
-                {projects.map((assignment, index) => (
-                    <AssignmentItem
-                        key={index}
-                        assignment={assignment}
-                        onClick={() => handleProjectClick(assignment.id)} // اینجا کلیک رو اضافه کردیم
+        <section
+            className="flex w-full p-3 gap-2 max-w-88 flex-col items-center flex-[1_0_0] border-[0.8px] border-solid border-neutralgray-2 rounded-lg ">
+            <HeadSection title={"پروژه‌ها"} onClick={handleAddProject}/>
+            {projects.length === 0 ? (
+                <div className="flex flex-1 flex-col items-center justify-center text-center mt-15">
+                    <img
+                        src="/src/class-page/assets/project-not-found.png"
+                        alt="No members"
+                        className="w-35 h-30 "
                     />
-                ))}
-            </div>
+                    <span className="text-caption-02 text-[0.9rem] text-neutral-400 mt-5 mb-10">! پروژه‌ای وجود ندارد</span>
+                </div>
+            ) : (
+                <div className="w-full max-h-150 overflow-y-auto flex flex-col gap-2 pr-5 relative">
+                    {projects.map((assignment, index) => (
+                        <AssignmentItem
+                            key={index}
+                            assignment={assignment}
+                            onClick={() => handleProjectClick(assignment.id)}
+                        />
+                    ))}
+                </div>
+            )}
         </section>
     );
 };
